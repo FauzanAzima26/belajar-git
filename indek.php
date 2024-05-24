6100c8a285a7fc460bff8a5521bb6efec22764a6
FauzanAzima26 <fauzannazimaaa123@gmail.com>
2024-05-24 23:37:16 +0700

    Add files via upload

---
+++ indek.php
@@ -0,0 +1,18 @@
+<?php
+echo "Selamat datang dipembelajaran PHP!";
+echo "<br>";
+print "Teknik Informatika";
+echo "<br>";
+
+$a = 10;
+$b = 20;
+echo $a + $b;
+echo "<br>";
+
+$namaDepan = "Fauzan";
+$namaBelakang = "Azima";
+echo $namaDepan . " " . $namaBelakang;
+echo "<br>";
+
+echo date("d-m- H:i:s");
+?>
